0931524d0093ca0393b6afc9a986b9da599bb61d
Nguyễn Trung Hiếu <17520480@gm.uit.edu.vn>
2020-10-28 08:44:13 +0700

    add side menu complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+import { SlideMenuModule } from 'primeng/slidemenu';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    SlideMenuModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MenuItem } from "primeng/api";
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  items: MenuItem[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Tra cứu nhân viên'
+      },
+      {
+        label: 'Tra cứu khách hảng'
+      },
+      {
+        label: 'Tra cứu phòng'
+      },
+      {
+        label: 'Thống kê doanh thu'
+      },
+      {
+        label: 'Thông tin cá nhân'
+      }
+    ]
   }
 
 }

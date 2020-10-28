6e6696c9e24ecf5010456badb241c028e856c3a5
Nguyễn Trung Hiếu <17520480@gm.uit.edu.vn>
2020-10-28 10:25:03 +0700

    commit pause

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,23 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 import { SlideMenuModule } from 'primeng/slidemenu';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {DropdownModule} from 'primeng/dropdown';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import {PasswordModule} from 'primeng/password';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {SliderModule} from 'primeng/slider';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {ToggleButtonModule} from 'primeng/togglebutton';
+import {CheckboxModule} from 'primeng/checkbox';
+import {CalendarModule} from 'primeng/calendar';
+import {ButtonModule} from 'primeng/button';
+
 import { TraCuuNhanVienComponent } from './components/tra-cuu-nhan-vien/tra-cuu-nhan-vien.component';
 import { TraCuuKhachComponent } from './components/tra-cuu-khach/tra-cuu-khach.component';
 import { TraCuuPhongComponent } from './components/tra-cuu-phong/tra-cuu-phong.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -26,6 +40,20 @@ import { TraCuuPhongComponent } from './components/tra-cuu-phong/tra-cuu-phong.c
     AppRoutingModule,
 
     SlideMenuModule,
+    InputTextareaModule,
+    InputNumberModule,
+    DropdownModule,
+    InputSwitchModule,
+    PasswordModule,
+    RadioButtonModule,
+    SliderModule,
+    SelectButtonModule,
+    ToggleButtonModule,
+    CheckboxModule,
+    CalendarModule,
+    ButtonModule,
+
+    FormsModule
     
   ],
   providers: [],
